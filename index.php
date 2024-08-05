6022e1a9fbeeee6bc97cf6c6adaa9843714f2884
Kai Hendry <hendry@iki.fi>
2024-08-05 17:06:37 +0100

    enable performance schema

--- index.php
+++ index.php
@@ -9,5 +9,18 @@ if ($mysqli->connect_error) {
     die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
-echo 'Connection successful!';
+// check performace_schema is enabled
+$performanceSchemaQuery =  "SHOW VARIABLES LIKE 'performance_schema';";
+$result = $mysqli->query($performanceSchemaQuery);
+if ($result) {
+    // Fetch the result
+    $row = $result->fetch_assoc();
+    echo "Variable: " . $row['Variable_name'] . "<br>";
+    echo "Value: " . $row['Value'] . "<br>";
+} else {
+    echo "Error: " . $conn->error;
+}
+
+$mysqli->close();
+
 ?>
