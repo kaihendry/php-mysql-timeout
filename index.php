d8b22456e9c20b65fc672ec4270e75880c9aed2f
Kai Hendry <hendry@iki.fi>
2024-09-20 15:46:31 +0100

    structured logging

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <?php
-$dbUser = getenv('DATABASE_USERNAME');
-$dbPassword = getenv('DATABASE_PASSWORD');
-$database = getenv('DATABASE_DATABASE');
 
-$mysqli = new mysqli('db', $dbUser, $dbPassword, $database);
+require 'vendor/autoload.php'; // This autoloads Monolog
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\JsonFormatter;
+use Monolog\ErrorHandler;
+
+$log = new Logger('name');
+$stderrHandler = new StreamHandler('php://stderr', Logger::DEBUG);
+$stderrHandler->setFormatter(new JsonFormatter());
+$log->pushHandler($stderrHandler);
+
+ErrorHandler::register($log);
+
+$mysqli = new mysqli('db', getenv('DATABASE_USERNAME'), getenv('DATABASE_PASSWORD'), getenv('DATABASE_DATABASE'));
 
 if ($mysqli->connect_error) {
-    die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+	throw new Exception('Database Connection Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
 
 // check performace_schema is enabled
@@ -18,7 +30,7 @@ if ($result) {
     echo "Variable: " . $row['Variable_name'] . "<br>";
     echo "Value: " . $row['Value'] . "<br>";
 } else {
-    echo "Error: " . $conn->error;
+	throw new Exception("Unable to fetch performance schema");
 }
 
 $mysqli->close();
