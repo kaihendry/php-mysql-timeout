e5a2c122a99597100a772fda8782e8e1293c2098
Kai Hendry <hendry@iki.fi>
2024-09-21 21:13:28 +0100

    fix lint

--- index.php
+++ index.php
@@ -15,19 +15,19 @@ $log->pushHandler($stderrHandler);
 ErrorHandler::register($log);
 
 set_exception_handler(function ($e) use ($log) {
-	$errId = uniqid();
-	$log->error($e->getMessage(), ['exception' => $e, 'errId' => $errId]);
-	http_response_code(500);
-	echo json_encode(['error' => 'An exception occurred', 'errId' => $errId]);
-	die();
+    $errId = uniqid();
+    $log->error($e->getMessage(), ['exception' => $e, 'errId' => $errId]);
+    http_response_code(500);
+    echo json_encode(['error' => 'An exception occurred', 'errId' => $errId]);
+    die();
 });
 
 set_error_handler(function ($errno, $errstr, $errfile, $errline) use ($log) {
-	$errId = uniqid();
-	$log->error($errstr, ['errId' => $errId, 'errno' => $errno, 'errfile' => $errfile, 'errline' => $errline]);
-	http_response_code(500);
-	echo json_encode(['error' => 'An error occurred', 'errId' => $errId]);
-	die();
+    $errId = uniqid();
+    $log->error($errstr, ['errId' => $errId, 'errno' => $errno, 'errfile' => $errfile, 'errline' => $errline]);
+    http_response_code(500);
+    echo json_encode(['error' => 'An error occurred', 'errId' => $errId]);
+    die();
 });
 
 throw new Exception('hiii');
@@ -36,4 +36,3 @@ throw new Exception('hiii');
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 $mysqli = mysqli_connect('db', getenv('DATABASE_USERNAME'), getenv('DATABASE_PASSWORD'), getenv('DATABASE_DATABASE'));
-
