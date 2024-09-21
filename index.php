72bef53cff63959e47ea3d97e680a5c05bcf961a
Kai Hendry <hendry@iki.fi>
2024-09-21 21:02:45 +0100

    error handling with php

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-set_exception_handler(function ($exception) {
-    http_response_code(500);
-    flush();
-});
-
-// Set a custom error handler
-set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-    http_response_code(500);
-    flush();
-});
-
-require 'vendor/autoload.php'; // This autoloads Monolog
+require 'vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -19,34 +8,32 @@ use Monolog\Formatter\JsonFormatter;
 use Monolog\ErrorHandler;
 
 $log = new Logger('name');
-$stderrHandler = new StreamHandler('php://stdout', Logger::DEBUG);
+$stderrHandler = new StreamHandler('php://stderr', Logger::DEBUG);
 $stderrHandler->setFormatter(new JsonFormatter());
 $log->pushHandler($stderrHandler);
 
-$log->info('starting up');
-
-
 ErrorHandler::register($log);
-// mysqli methods throw exceptions on error
+
+set_exception_handler(function ($e) use ($log) {
+	$errId = uniqid();
+	$log->error($e->getMessage(), ['exception' => $e, 'errId' => $errId]);
+	http_response_code(500);
+	echo json_encode(['error' => 'An exception occurred', 'errId' => $errId]);
+	die();
+});
+
+set_error_handler(function ($errno, $errstr, $errfile, $errline) use ($log) {
+	$errId = uniqid();
+	$log->error($errstr, ['errId' => $errId, 'errno' => $errno, 'errfile' => $errfile, 'errline' => $errline]);
+	http_response_code(500);
+	echo json_encode(['error' => 'An error occurred', 'errId' => $errId]);
+	die();
+});
+
+throw new Exception('hiii');
+// trigger_error('Error', E_USER_ERROR);
+
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = new mysqli('db', getenv('DATABASE_USERNAME'), getenv('DATABASE_PASSWORD'), getenv('DATABASE_DATABASE'));
+$mysqli = mysqli_connect('db', getenv('DATABASE_USERNAME'), getenv('DATABASE_PASSWORD'), getenv('DATABASE_DATABASE'));
 
-if ($mysqli->connect_error) {
-    throw new Exception('Database Connection Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-}
-
-// check performace_schema is enabled
-$performanceSchemaQuery = "SHOW VARIABLES LIKE 'performance_schema';";
-$result = $mysqli->query($performanceSchemaQuery);
-if ($result) {
-    $row = $result->fetch_assoc();
-    echo "Variable: " . $row['Variable_name'] . "<br>";
-    echo "Value: " . $row['Value'] . "<br>";
-} else {
-    throw new Exception("Unable to fetch performance schema");
-}
-
-$mysqli->close();
-
-$log->info('database connection successful');
